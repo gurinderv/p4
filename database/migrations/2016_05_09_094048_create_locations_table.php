09636a7a4456a80c6736fb3b3ec08c0c74917ff7
Gurinder Virdi <gurinder@gurindervirdi.com>
2016-05-09 11:01:37 -0700

    location migration complete

---
+++ database/migrations/2016_05_09_094048_create_locations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+
+      Schema::create('locations', function (Blueprint $table) {
+
+          # Increments method will make a Primary, Auto-Incrementing field.
+        $table->increments('id');
+
+          # This generates two columns: `created_at` and `updated_at` to
+          # keep track of changes to a row
+        $table->timestamps();
+
+        $table->string('location_name');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::drop('locations');
+    }
+}
